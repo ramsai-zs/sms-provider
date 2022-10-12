2e89db45ef3343f0f63d8f2421c39fb3c2695fe2
rlingamp <ramsai_lingampalli@mcafee.com>
2022-10-12 15:06:25 +0530

    add new migrations

--- migrations/000_all.go
+++ migrations/000_all.go
@@ -8,6 +8,8 @@ import (
 func All() map[string]dbmigration.Migrator{
 	return map[string]dbmigration.Migrator{
 	
-		"20221012142722": K20221012142722{},
+		"20221012142722": K20221012142722{},	
+		"20221012144448": K20221012144448{},	
+		"20221012150349": K20221012150349{},
 	}
 }
