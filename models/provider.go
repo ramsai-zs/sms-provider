c629b19c2186f878169cf7bedd522d793109b346
rlingamp <ramsai_lingampalli@mcafee.com>
2022-10-12 12:34:39 +0530

    created model for the sms provider

---
+++ models/provider.go
@@ -0,0 +1,10 @@
+package models
+
+import "github.com/google/uuid"
+
+type Provider struct {
+	ID           uuid.UUID `json:"Id"`
+	URL          string    `json:"URL"`
+	ChannelRefID string    `json:"channelRefID"`
+	Name         string    `json:"name"`
+}
