59e885e64aa6e4113a055341d52fb6efb046184c
rlingamp <ramsai_lingampalli@mcafee.com>
2022-10-12 15:10:15 +0530

    removed message model from provider

--- models/provider.go
+++ models/provider.go
@@ -2,8 +2,6 @@ package models
 
 import (
 	"github.com/google/uuid"
-
-	"sms-provider/types"
 )
 
 type Provider struct {
@@ -11,14 +9,4 @@ type Provider struct {
 	URL          string    `json:"URL"`
 	ChannelRefID string    `json:"channelRefID"`
 	Name         string    `json:"name"`
-}
-
-type Message struct {
-	ID            uuid.UUID    `json:"id"`
-	Message       string       `json:"message"`
-	Number        string       `json:"number"`
-	Transactional string       `json:"transactional"`
-	ProviderRefID uuid.UUID    `json:"ProviderRefID"`
-	Status        types.Status `json:"status"`
-	DeliveredTime string       `json:"deliveredTime"`
 }
