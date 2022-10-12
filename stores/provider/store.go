9ba3d70a31dc0d54557116d3397f0ea30ae3a3bc
rlingamp <ramsai_lingampalli@mcafee.com>
2022-10-12 15:36:57 +0530

    initialized store for the provider

--- stores/provider/store.go
+++ stores/provider/store.go
@@ -1,1 +1,34 @@
 package provider
+
+import (
+	"developer.zopsmart.com/go/gofr/pkg/gofr"
+	"sms-provider/models"
+	"sms-provider/stores"
+)
+
+type store struct{}
+
+// New factory function to initialize store.
+func New() stores.Provider {
+	return store{}
+}
+
+func (s store) Create(ctx *gofr.Context, message models.Provider) (models.Provider, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s store) GetByID(ctx *gofr.Context, id string) (models.Provider, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s store) PUT(ctx *gofr.Context, id string) (models.Provider, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s store) Delete(ctx *gofr.Context, id string) error {
+	//TODO implement me
+	panic("implement me")
+}
