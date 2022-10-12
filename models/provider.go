28aa4d4640cffd54e6127d68fbe3c2e4444fdf27
rlingamp <ramsai_lingampalli@mcafee.com>
2022-10-12 13:01:21 +0530

    add models for message and change in json for provider ID

--- models/provider.go
+++ models/provider.go
@@ -1,10 +1,24 @@
 package models
 
-import "github.com/google/uuid"
+import (
+	"github.com/google/uuid"
+
+	"sms-provider/types"
+)
 
 type Provider struct {
-	ID           uuid.UUID `json:"Id"`
+	ID           uuid.UUID `json:"id"`
 	URL          string    `json:"URL"`
 	ChannelRefID string    `json:"channelRefID"`
 	Name         string    `json:"name"`
+}
+
+type Message struct {
+	ID            uuid.UUID    `json:"id"`
+	Message       string       `json:"message"`
+	Number        string       `json:"number"`
+	Transactional string       `json:"transactional"`
+	ProviderRefID uuid.UUID    `json:"ProviderRefID"`
+	Status        types.Status `json:"status"`
+	DeliveredTime string       `json:"deliveredTime"`
 }
