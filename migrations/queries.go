316eb946f0b736e02239d7039062b0ca6a692663
pmedapat <pavan_medapati@mcafee.com>
2022-10-12 16:17:37 +0530

    Queries were modified

--- migrations/queries.go
+++ migrations/queries.go
@@ -2,11 +2,10 @@ package migrations
 
 const (
 	createTableProvider = `CREATE TABLE IF NOT EXISTS Provider(
-      ID VARCHAR(40) NOT NULL UNIQUE,
+      ID VARCHAR(40) PRIMARY KEY,
       URL VARCHAR(50) NOT NULL,
 	  ChannelRefID VARCHAR(40) NOT NULL,
-      Name VARCHAR(80) NOT NULL,
-	  PRIMARY KEY(ID))`
+      Name VARCHAR(80) NOT NULL)`
 
 	dropTableProvider = `DROP TABLE IF EXISTS Provider`
 
@@ -24,8 +23,7 @@ const (
 	  Number VARCHAR(20) NOT NULL,
       Transactional VARCHAR(50) NOT NULL,
 	  Status status NOT NULL DEFAULT 'NO',
-      Delivered_time TIMESTAMPZ,
-      PRIMARY KEY(ID))`
+      Delivered_time TIMESTAMP)`
 
 	dropTableMessage = `DROP TABLE IF EXISTS Message`
 )
