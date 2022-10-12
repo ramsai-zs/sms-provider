c07410b8488f89287405134754b19e76ef7ad02e
rlingamp <ramsai_lingampalli@mcafee.com>
2022-10-12 13:01:53 +0530

    add enum for status

--- types/status.go
+++ types/status.go
@@ -1,1 +1,38 @@
 package types
+
+import (
+	"encoding/json"
+	"strings"
+)
+
+type Status string
+
+const (
+	yes Status = "YES"
+	no  Status = "NO"
+)
+
+// UnmarshalJSON This custom Unmarshal method is written to validate enum status field.
+func (s *Status) UnmarshalJSON(b []byte) error {
+	var temp string
+
+	err := json.Unmarshal(b, &temp)
+	if err != nil {
+		return &json.UnmarshalTypeError{Field: "status"}
+	}
+
+	temp = strings.TrimSpace(temp)
+
+	switch temp {
+	case "YES":
+		*s = yes
+	case "NO":
+		*s = no
+	case "":
+		*s = ""
+	default:
+		return &json.UnmarshalTypeError{Field: "status"}
+	}
+
+	return nil
+}
